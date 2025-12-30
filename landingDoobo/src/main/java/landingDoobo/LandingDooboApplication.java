b1a10da3fbc77c62c4c9f68393706acaed852549
KDH <kjo4202@naver.com>
2025-12-31 00:35:56 +0900

    -- 배포 버그 수정

--- landingDoobo/src/main/java/landingDoobo/LandingDooboApplication.java
+++ landingDoobo/src/main/java/landingDoobo/LandingDooboApplication.java
@@ -2,12 +2,18 @@ package landingDoobo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class LandingDooboApplication {
+public class LandingDooboApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(LandingDooboApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(LandingDooboApplication.class, args);
 	}
-
 }
